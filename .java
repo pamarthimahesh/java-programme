e1d0612a06ec6ca90b5cac687c0d81292a221a73
pamarthimahesh <maheshpamarthi8888@gmail.com>
2025-06-29 22:43:55 +0530

    Update .java

--- .java
+++ .java
@@ -1,1 +1,68 @@
+1.public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
 
+output  : hello world
+
+
+  2. #enter two numbers
+
+  import java.util.scanner;
+  class add two numbers
+  {
+  public static void main(string args[])
+    {
+    scanner input = new scanner(system.in);
+    int a= input.next int();
+    System.Output.Println("enter firts number");
+    int b= input.next int();
+    int c=a+b;
+    System.Output.Println("enter second number");
+    input.close()
+  }
+}
+
+
+output  :  enter firts number 12
+    enter second number  30
+  c=42
+
+  3.#package pkg1basic program
+  public class emp 1 {
+   public static void main(string args[])
+    {
+      System.Output.Println("E id:101");
+      System.Output.Println("Employee Name: Mahesh");
+      System.Output.Println("job :manager");
+      System.Output.Println("exp:3years");
+      System.Output.Println("salary :80000");
+      System.Output.Println("company :wipro");
+      System.Output.Println("branch/location : delhi");
+      System.Output.Println("date of joining :01-jan-2025");
+   }
+}
+
+
+4. #display your name
+
+  package pkg1basic program;
+  public class test1 {
+     public static void main(string args[])
+    {
+       System.Output.Println("my name is mahesh");
+    }
+  }
+
+      
+    
+    
+  
+     
+      
+     
+  
+
+  
+    
