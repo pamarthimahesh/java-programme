6638b9df33204949211c9eca3c2ee327c82e1369
pamarthimahesh <maheshpamarthi8888@gmail.com>
2025-07-06 00:00:42 +0530

    Update .java

--- .java
+++ .java
@@ -57,11 +57,148 @@ output  :  enter firts number 12
 
       
     
+ 5.public class qotremin1 
+ {
+   public static void main(string[]args)  
+   {
+   //quotient and remainder:
+   System.Output.Println("9/10=  "+9%10  +  "(9%10));
+ System.Output.Println("9876/10=  "+9876%10  +  "(9876%10));
+ System.Output.Println("9876/100=  "+9876%100  +  "(9876%100));
+ System.Output.Println("9876/1000=  "+9876%1000  +  "(9876%1000));
+   }
+ }
+
+6.packagepk4arithop1;
+public class arthemetic0 {
+  public static void main(string[]args)  {
+    //10 ,2
+    
+   System.Output.Println("10+2=  "+  (10+2)+  "10-2=  "+  (10-2));
+     System.Output.Println("10*2=  "+  (10*2)+"10/2=  "+  (10/2));
+    
+  }
+}
+
+
+7.packagepk4arithop1;
+public class arthemetic1 {
+  public static void main(string[]args)  {
+    int a,b;
+    a=10,b=2;
+    int sum,difference,product,quotient,reminder;
+    sum=a+b;difference=a-b;
+    product=a*b;
+    quotient=a/b;remaindera%b;
+     System.Output.Println("sum=  "+ sum+  "difference=  "+  difference);
+     System.Output.Println("product=  "+ poduct+ );
+     System.Output.Println("quotient=  "+ quotient+  " remainder=  "+  remainder);
+  }
+}
+
+
+8.// This is a comment//
+System.out.println("Hello World");
+
+9.//multiple variables
+  public class Main {
+  public static void main(String[] args) {
+    int x = 10, y = 25, z = 50;
+    System.out.println(x + y + z);
+  }
+}
+
+10.public class Main {
+  public static void main(String[] args) {
+    String name = "pamarthi mahesh";
+    System.out.println(name);
+  }
+}
+
+
+
+11.//arthematic operatots //
+  public class Main {
+  public static void main(String[] args) {
+    int x = 8;
+    int y = 6;
+    System.out.println(x +y); 
+    System.out.println(x -y); 
+    System.out.println(x *y); 
+    System.out.println(x/y);
+    System.out.println(x%y); 
+  }
+}
+
+ 
+
+
+12.//displau first name and last  name//
+  public class Main {
+  public static void main(String[] args) {
+    String firstName = "mahesh ";
+    String lastName = "paqmarthi";
+    String fullName = firstName + lastName;
+    System.out.println(fullName);  
+  }
+
+  13. //multiple line comments
+  public class Main {
+  public static void main(String[] args) {
+    /* The code below will print the words i  am pamarthi.mahesh .i am studying btech 3rd year cse
+    to the screen, and it is amazing */
+    System.out.println("i  am pamarthi.mahesh .i am studying btech 3rd year cse");
+  }
+}
+
+
+
+  14.//student data
+  public class Main {
+  public static void main(String[] args) {
+    // Student data
+    String studentName = "pamarthi.mahesh";
+    int studentID = 108;
+    int studentAge = 21;
+    float studentFee = 90.00f;
+    char studentGrade = 'A';
+  
+    System.out.println("Student name: " + studentName);
+    System.out.println("Student id: " + studentID);
+    System.out.println("Student age: " + studentAge);
+    System.out.println("Student fee: " + studentFee);
+    System.out.println("Student grade: " + studentGrade);
+  }
+}
+
+
+
+
+
+
+
+
+    
     
   
-     
-      
-     
+  
+
+  
+    
+
+
+    
+    
+  
+  
+
+  
+    
+
+
+    
+    
+  
   
 
   
